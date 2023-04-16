8cc22e6b9654f6853952cfc9c345e989973114ae
ejdam87 <adam.dzadon@gmail.com>
2023-04-16 15:38:10 +0200

    Perceptron algorithm add

--- linear-models/perceptron.py
+++ linear-models/perceptron.py
@@ -1,6 +1,9 @@
 import random
 from vector import Vector
-from typing import Callable
+from typing import Callable, TypeVar
+
+Label = TypeVar( "T" )
+
 
 class Perceptron:
 
@@ -8,9 +11,11 @@ class Perceptron:
                   dim: int,
                   activation: Callable[ [ float ], float ]=None ) -> None:
 
+        self.learning_rate = 1
+
         self.dim = dim
         self.val = random.random( )
-        self.weights = Vector( [ random.random( ) for _ in range( dim + 1 ) ] )
+        self.weights = Vector( [ 0 for _ in range( dim + 1 ) ] )
         self.activation = activation
 
     def set_weights( self, v: Vector ) -> None:
@@ -34,10 +39,32 @@ class Perceptron:
     def weighted_sum( self, v: Vector ) -> float:
         assert len( v ) == len( self.weights ) - 1, "Invalid vector size"
 
-        return v.extended( -1 ) * self.weights
+        return v.extended( 1 ) * self.weights
 
     def compute( self, v: Vector ) -> None:
         assert self.activation is not None, "Missing activation function"
 
         sm = self.weighted_sum( v )
         self.val = self.activation( sm )
+
+    def learn( self, data: list[ Vector ], labels: list[ Label ] ) -> None:
+        assert len( data ) == len( labels )
+
+        ## We want consistent separator
+        converged = False
+
+        ## It is PROVEN that perceptron learning algorithm will always converge
+        while not converged:
+
+            converged = True
+            for sample, label in zip( data, labels ):
+                predicted = self.predict( sample )
+                
+                if ( predicted - label ) != 0:
+                    converged = False
+
+                self.weights = self.weights - self.learning_rate * ( predicted - label ) * sample.extended( 1 )
+
+
+    def predict( self, v: Vector ) -> Label:
+        return 1 if self.weighted_sum( v ) >= 0 else 0
